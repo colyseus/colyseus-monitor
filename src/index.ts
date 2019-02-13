d661158fd22ce7488107050badba61d137f1ad2a
Endel Dreyer <endel.dreyer@gmail.com>
2019-02-13 21:15:09 -0200

    upgrade dependencies.

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import './ext/Room';
 
 const frontendDirectory = path.resolve(__dirname, "..", "lib", "static");
 
-export function monitor (server: Server) {
+export function monitor (server: Server): express.Router {
     const router = express.Router();
     router.use('/', express.static(frontendDirectory));
     router.use('/api', getAPI(server));
